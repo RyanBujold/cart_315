30e6af57a3e26e21a0c0b59d1937ca5e34ea138c
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2025-03-06 20:06:28 -0500

    Feat: added a moving racer with collision to bouncers

--- Projects/prototype_project_v1/Assets/Scripts/Racer.cs
+++ Projects/prototype_project_v1/Assets/Scripts/Racer.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Racer : MonoBehaviour
 {
+    public KeyCode driveKey;
+    public float driveSpeed = 0;
+
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // When drive key pressed, move the racer forward
+        if(Input.GetKey(driveKey)){
+            rb.velocity += new Vector3(0,0,driveSpeed);
+        }
+
     }
+
 }
